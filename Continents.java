3a9ed2213823e7141b29364afb038b78bee465db
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-15 09:10:26 -0600

    ...

--- Continents.java
+++ Continents.java
@@ -1,5 +1,15 @@
 public class Continents {
     public static void main(String[] args) {
+
+        int continent = 4;
+        switch (continent) {
+            case 1:
+                
+                break;
         
+            default:
+                break;
+        }
+
     }
 }

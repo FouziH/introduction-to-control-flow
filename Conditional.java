6ad9af2cdf5526e3e9690966e032851251021a75
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-15 07:04:58 -0600

    ....

--- Conditional.java
+++ Conditional.java
@@ -1,7 +1,15 @@
-public class Condtional {
+public class Conditional {
 
 boolean isFilled;
 double billAmount;
+String shipping;
+String couponCode;
+
+//Class constructor method
+//This constructor method takes fee arguments 
+public Conditional (boolean filled, double cost, String shippingMethod, String coupon) {
+
+}
 
     public static void main(String[] args) {
 

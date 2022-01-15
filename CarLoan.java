f6ead0e02040a4d7e4cd685aa3593900b897876d
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-15 08:37:05 -0600

    ...

--- CarLoan.java
+++ CarLoan.java
@@ -1,6 +1,8 @@
 public class CarLoan {
      int carLoan = 10000;
      int loanLength = 3;
+     int interestRate = 5;
+     int downPayment = 2000;
     public static void main(String[] args){
 
     }

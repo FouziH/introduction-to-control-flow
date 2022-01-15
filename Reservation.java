6f66dc83aca8f36e247ea09d3fff5d61cd486342
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-15 07:53:55 -0600

    ....

--- Reservation.java
+++ Reservation.java
@@ -6,7 +6,7 @@ public class Reservation {
     public Reservation(int count, int capacity, boolean open) {
 
         if(count < 1 || count > 8) {
-            
+            System.out.println("Invalid reservation!");
         }
         guestCount = count;
         restaurantCapacity = capacity;

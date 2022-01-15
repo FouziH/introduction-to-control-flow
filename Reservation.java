d03bb228dc8e932b996a1a3d5f10ec3975a551d6
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-15 07:52:40 -0600

    ....

--- Reservation.java
+++ Reservation.java
@@ -4,6 +4,10 @@ public class Reservation {
     boolean isRestaurantOpen;
     boolean isConfirmed;
     public Reservation(int count, int capacity, boolean open) {
+
+        if(count < 1 || count > 8) {
+            
+        }
         guestCount = count;
         restaurantCapacity = capacity;
         isRestaurantOpen = open;

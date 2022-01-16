5cd51e224d2588647c9e963ba773e67a9c94cd3c
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-16 06:57:39 -0600

    ...

--- ToDos.java
+++ ToDos.java
@@ -4,7 +4,14 @@ import java.util.ArrayList;
  class ToDos {
 
     public static void main(String[] args) {
-        ArrayList<String> todo = new ArrayList<String>();
+        ArrayList<String> toDoList = new ArrayList<String>();
+
+        //Adding toDo1 to the ArrayList
+        String toDo1 = "Eat Healthy";
+        String toDo2  = "Go my home show appointmenst";
+        String toDo3 = "Finish the Java CodeAcademy Course by Monday";
+
+        toDoList.add(toDo1);
     }
 
     

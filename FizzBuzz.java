77090e5cc01c6ad774138773ece42f6a30704a9a
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-16 09:33:30 -0600

    ...

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,8 +1,19 @@
-public class FizzBuzz {
-
-    public static void main(String[] args) {
-     for(int i = 1; i <= 100; i++) {
+class FizzBuzz {
+  public static void main(String[] args) {
+    for(int i = 1; i <= 100; i++) {
       int counter = i;
-      System.out.println(counter);
+    
+      if(counter % 3 == 0) {
+        System.out.println("Fizz");
+
+      } else if(counter % 5 == 0) {
+         System.out.println("Buzz");
+
+      } else if(counter % 3 == 0 && counter % 5 == 0 ){
+          System.out.println("FizzBuzz");
+      } else {
+          System.out.println(counter);
+      }
     }
+  }
 }

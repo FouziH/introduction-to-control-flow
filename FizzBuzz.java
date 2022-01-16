4d61895a45fef8a746bfb8f12e5e81082d9e0140
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-16 09:26:50 -0600

    ...

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,4 +1,8 @@
 public class FizzBuzz {
-    
-    
+
+    public static void main(String[] args) {
+     for(int i = 1; i <= 100; i++) {
+      int counter = i;
+      System.out.println(counter);
+    }
 }

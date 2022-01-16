e0f325aa4b2179b5351bcb6b1db36e1f47ba018b
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-15 20:15:03 -0600

    ...

--- NewsFeed.java
+++ NewsFeed.java
@@ -1,18 +1,21 @@
 public class NewsFeed {
 
-
     public NewsFeed() {
 
     }
 
-    //Create getTopics() method below
+    // Create getTopics() method below
     public String[] getTopics() {
-        String[] topics = {"Opinion", "Tech", "Science", "Health"};
+        String[] topics = { "Opinion", "Tech", "Science", "Health" };
 
         return topics;
     }
 
     public static void main(String[] args) {
+        NewsFeed sampleFeed = new NewsFeed();
+
+        String[] topics = sampleFeed.getTopics();
+        System.out.println(topics);
 
     }
 }

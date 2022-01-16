ad48b1ae09a5807518978281afddaa8fe4331453
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-16 08:18:05 -0600

    ...

--- LuckyFive.java
+++ LuckyFive.java
@@ -1,7 +1,19 @@
 import java.util.Random;
 
 public class LuckyFive {
-
+    public static void main(String[] args) {
     
+    // Creating a random number generator
+    Random randomGenerator = new Random();
+    
+    // Generate a number between 1 and 6
+    int dieRoll = randomGenerator.nextInt(6) + 1;
+
+    // Repeat while roll isn't 5
+    
+    
+  }
+
+
     
 }

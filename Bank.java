11a3a7bb9051560d633fa1aa96a96764a36c39e0
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-17 09:39:00 -0600

    ...

--- Bank.java
+++ Bank.java
@@ -5,6 +5,11 @@ public class Bank {
 
     public Bank() {
         accountOne = new CheckingAccount("Zue", 100);
+        accountOne = new CheckingAccount("Hades", 200);
+    }
+
+    public static void main(String[] args) {
+        
     }
     
 }

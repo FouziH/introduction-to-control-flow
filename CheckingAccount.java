b5c73296b5244d0adb31a7a3588e1b51c6879f50
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-17 09:43:03 -0600

    ...

---
+++ CheckingAccount.java
@@ -0,0 +1,10 @@
+public class CheckingAccount {
+    private String name;
+    private String balance;
+
+    public CheckingAccount(String inputName, String inputBalance) {
+        name = inputName;
+        balance = inputBalance;
+    }
+    
+}

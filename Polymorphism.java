1f687c1f225436f7acef73f5df92639ccc62bd0f
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-17 19:36:46 -0600

    ...

---
+++ Polymorphism.java
@@ -0,0 +1,24 @@
+public class Polymorphism {
+    
+}
+
+//Introduction Polymorphism
+
+/* 
+
+In java, if Orange is a Fruit through inheritance, you can then use Orange in the same contexts as Fruit like this:
+
+String makeJuice(Fruit fruit) {
+    return "Apple juice and " + fruit.squeexe();
+}
+
+//inside main()
+
+Orange orange = new Orange();
+
+System.out.println(juicer.makeJuice(orange));
+
+
+
+
+*/
